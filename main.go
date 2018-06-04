fc66e673fb29c222cf9f2edafb8768e5249eff20
Daniel Brunelle <daniel.brunelle@me.com>
2018-06-04 00:01:32 -0600

    Update html content and routes

--- main.go
+++ main.go
@@ -10,13 +10,32 @@ import (
 func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("method:", r.Method) //get request method
 	if r.Method == "GET" {
-		t, _ := template.ParseFiles("views/html/index.html")
+		t, _ := template.ParseFiles("views/html/about.html")
+		t.Execute(w, nil)
+	}
+}
+
+func projects(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method) //get request method
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("views/html/projects.html")
+		t.Execute(w, nil)
+	}
+}
+
+func hireMe(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method) //get request method
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("views/html/hire_me.html")
 		t.Execute(w, nil)
 	}
 }
 
 func main() {
 	http.HandleFunc("/", index)              // setting router rule
+	http.HandleFunc("/about", index)         // setting router rule
+	http.HandleFunc("/projects", projects)   // setting router rule
+	http.HandleFunc("/hire_me", hireMe)      // setting router rule
 	err := http.ListenAndServe(":9090", nil) // setting listening port
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
